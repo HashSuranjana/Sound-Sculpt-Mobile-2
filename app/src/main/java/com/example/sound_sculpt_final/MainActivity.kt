3cce677f8e37f8671cb3cb23e4bab996ef43d015
Hasitha Suranjana <hasithasuranjana12@gmail.com>
2024-02-14 20:09:21 +0530

    Updated files

--- app/src/main/java/com/example/sound_sculpt_final/MainActivity.kt
+++ app/src/main/java/com/example/sound_sculpt_final/MainActivity.kt
@@ -10,37 +10,25 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.fragment.app.Fragment
+import com.example.sound_sculpt_final.databinding.ActivityMainBinding
+import com.example.sound_sculpt_final.databinding.ActivityRegisterBinding
 import com.example.sound_sculpt_final.ui.theme.Sound_Sculpt_FinalTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            Sound_Sculpt_FinalTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun replaceFragment(fragment :Fragment){
+
+        
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Sound_Sculpt_FinalTheme {
-        Greeting("Android")
-    }
-}
