f934d20e72e0842ca8ed6c52583fa62c70143b8a
Hasitha Suranjana <hasithasuranjana12@gmail.com>
2024-02-14 19:00:33 +0530

    Updated files

--- app/src/main/java/com/example/sound_sculpt_final/Sign_in.kt
+++ app/src/main/java/com/example/sound_sculpt_final/Sign_in.kt
@@ -37,7 +37,7 @@ class Sign_in : AppCompatActivity() {
 
                 firebaseAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener {
                     if (it.isSuccessful){
-                        val intent = Intent(this,ContactsContract.Profile::class.java)
+                        val intent = Intent(this,MainActivity::class.java)
                         startActivity(intent)
                     }else{
                         Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
