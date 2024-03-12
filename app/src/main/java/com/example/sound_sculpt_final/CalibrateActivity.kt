bb98a5b758d53fe69f6f12f68e4505fc400f874c
Hasitha Suranjana <hasithasuranjana12@gmail.com>
2024-03-12 07:44:27 +0530

    updated files

--- app/src/main/java/com/example/sound_sculpt_final/CalibrateActivity.kt
+++ app/src/main/java/com/example/sound_sculpt_final/CalibrateActivity.kt
@@ -24,11 +24,26 @@ class CalibrateActivity : AppCompatActivity() {
         val mediaPlayer = MediaPlayer()
         mediaPlayer.setDataSource(audioFilePath)
         mediaPlayer.prepare()
-        val duration =
-            mediaPlayer.duration // Get the duration of the recorded audio in milliseconds
+        val duration = mediaPlayer.duration // Get the duration of the recorded audio in milliseconds
 
         val segmentDuration = duration / 7 // Split the duration into 7 segments
 
         // Calculate decibel levels for each segment
+        for (i in 0 until 7) {
+            val start = i * segmentDuration
+            val end = if (i == 6) duration else (i + 1) * segmentDuration
+            val decibelLevel = calculateDecibelLevel(audioFilePath, start, end)
+            // Do something with the decibel level for this segment
+            println("Decibel level for segment $i: $decibelLevel")
+        }
+
+        mediaPlayer.release()
+    }
+
+    private fun calculateDecibelLevel(audioFilePath: String, start: Int, end: Int): Double {
+        // Implement this method to calculate decibel level for a given segment of audio
+        // You can use libraries like TarsosDSP for audio processing
+        // This is just a placeholder method
+        return 0.0 // Placeholder value
     }
 }
