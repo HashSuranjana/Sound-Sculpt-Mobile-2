dd36dba8864ee77ebd6d388b5e67ff2596a35e7e
Hasitha Suranjana <hasithasuranjana12@gmail.com>
2024-03-25 07:38:02 +0530

    Dependecies optimized

--- app/src/test/java/com/example/sound_sculpt_final/RegisterTest.kt
+++ app/src/test/java/com/example/sound_sculpt_final/RegisterTest.kt
@@ -1,14 +1,6 @@
 package com.example.sound_sculpt_final
 
-import com.google.firebase.auth.FirebaseAuth
-import org.junit.Assert.assertFalse
-import org.junit.Assert.assertTrue
-import org.junit.Before
-import org.junit.Test
-import org.mockito.Mock
-import org.mockito.Mockito.`when`
-import org.mockito.Mockito.mock
-import org.mockito.MockitoAnnotations
+
 
 class RegisterTest {
 
