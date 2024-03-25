88c1c0d0440de7f646436c7e486760ec9f6337a1
Hasitha Suranjana <hasithasuranjana12@gmail.com>
2024-03-25 07:30:43 +0530

    some other Testings

--- app/src/test/java/com/example/sound_sculpt_final/Sign_inTest.kt
+++ app/src/test/java/com/example/sound_sculpt_final/Sign_inTest.kt
@@ -1,20 +1,10 @@
 package com.example.sound_sculpt_final
 
-import android.app.Activity.RESULT_OK
-import android.content.Intent
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.common.api.ApiException
-import com.google.common.base.Verify.verify
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.GoogleAuthProvider
-import org.junit.Assert.assertEquals
-import org.junit.Before
-import org.junit.Test
-import org.mockito.Mockito.mock
 
-class Sign_inTest {
+
+
+
+class SignInActivityTest {
 
 
 }
